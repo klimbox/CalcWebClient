8753402b450b587410f1d7fec8d83dcde8fa90fe
roma <chuprina.r.v@gmail.com>
2017-11-12 19:16:07 +0300

    переделал тесты под Nunit

--- AT/UnitTest1.cs
+++ AT/UnitTest1.cs
@@ -1,76 +1,74 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Threading;
 
 namespace AT
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
         private static ChromeDriver drivers = new ChromeDriver();
-        [ClassCleanup]
+        [OneTimeTearDown]
         public static void ClassCleanup()
         {
             drivers.Quit();
         }
 
-        [TestInitialize]
+        [SetUp]
         public void TestInit()
         {
-            drivers.Navigate().GoToUrl("file:///D:/Client%20Web/CalcWebClient/CalcWebClient/WebCalculator.html");
+            drivers.Navigate().GoToUrl("file:///E:/CalcWebClient/CalcWebClient/WebCalculator.html");
         }
-        [DataTestMethod]
-        [DataRow("btn7")]
-        [DataRow("btn8")]
-        [DataRow("btn9")]
-        [DataRow("btnPlus")]
-        [DataRow("btn4")]
-        [DataRow("btn5")]
-        [DataRow("btn6")]
-        [DataRow("btnMinus")]
-        [DataRow("btn1")]
-        [DataRow("btn2")]
-        [DataRow("btn3")]
-        [DataRow("btnMult")]
-        [DataRow("btnDot")]
-        [DataRow("btn0")]
-        [DataRow("btnEq")]
-        [DataRow("btnDiv")]
+        [TestCase("btn7")]
+        [TestCase("btn8")]
+        [TestCase("btn9")]
+        [TestCase("btnPlus")]
+        [TestCase("btn4")]
+        [TestCase("btn5")]
+        [TestCase("btn6")]
+        [TestCase("btnMinus")]
+        [TestCase("btn1")]
+        [TestCase("btn2")]
+        [TestCase("btn3")]
+        [TestCase("btnMult")]
+        [TestCase("btnDot")]
+        [TestCase("btn0")]
+        [TestCase("btnEq")]
+        [TestCase("btnDiv")]
+        [Test]
         public void SimpleCheckBtn(string id)
         {
             IWebElement chromeWebElement = drivers.FindElementById(id);
             Assert.AreEqual(true, chromeWebElement.Displayed);
         }
-        [DataTestMethod]
-        [DataRow("btn7", "7")]
-        [DataRow("btn8", "8")]
-        [DataRow("btn9", "9")]
-        [DataRow("btn4", "4")]
-        [DataRow("btn5", "5")]
-        [DataRow("btn6", "6")]
-        [DataRow("btn1", "1")]
-        [DataRow("btn2", "2")]
-        [DataRow("btn3", "3")]
-        [DataRow("btnDot", ",")]
-        [DataRow("btn0" , "0")]
+        [TestCase("btn7", "7")]
+        [TestCase("btn8", "8")]
+        [TestCase("btn9", "9")]
+        [TestCase("btn4", "4")]
+        [TestCase("btn5", "5")]
+        [TestCase("btn6", "6")]
+        [TestCase("btn1", "1")]
+        [TestCase("btn2", "2")]
+        [TestCase("btn3", "3")]
+        [TestCase("btnDot", ",")]
+        [TestCase("btn0" , "0")]
+        [Test]
         public void SimpleClicBtn(string id, string exp)
         {
             drivers.FindElementById(id).Click();
             Assert.AreEqual(exp, drivers.FindElementById("aTxt").GetAttribute("value"));
         }
-        [DataTestMethod]
-        [DataRow("btn7", "777")]
-        [DataRow("btn8", "888")]
-        [DataRow("btn9", "999")]
-        [DataRow("btn4", "444")]
-        [DataRow("btn5", "555")]
-        [DataRow("btn6", "666")]
-        [DataRow("btn1", "111")]
-        [DataRow("btn2", "222")]
-        [DataRow("btn3", "333")]
-        [DataRow("btnDot", ",,,")]
-        [DataRow("btn0", "000")]
+        [TestCase("btn7", "777")]
+        [TestCase("btn8", "888")]
+        [TestCase("btn9", "999")]
+        [TestCase("btn4", "444")]
+        [TestCase("btn5", "555")]
+        [TestCase("btn6", "666")]
+        [TestCase("btn2", "222")]
+        [TestCase("btn3", "333")]
+        [TestCase("btnDot", ",,,")]
+        [TestCase("btn0", "000")]
+        [Test]
         public void CoblexClicBtn(string id, string exp)
         {
             for (int i = 0; i < 3; i++)
