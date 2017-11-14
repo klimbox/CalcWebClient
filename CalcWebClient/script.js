6f1369a440ccfc9ee175654db73cc546bfdcbc06
Клименко Владимир <klimbox@gmail.com>
2017-11-14 23:41:48 +0200

    19.4: refactoring. Work with the server is in separate class now

--- CalcWebClient/script.js
+++ CalcWebClient/script.js
@@ -1,5 +1,26 @@
-﻿var aStr;
+﻿'use strict';
+
+class ServerCalc {
+    constructor(url) {
+        this.url = url;
+    }
+
+    Calculate(num1, num2, opr) {
+        if (opr === "+") { opr = "plus"; }
+        var req = "num1=" + num1 + "&num2=" + num2 + "&opr=" + opr;
+        var rr = new XMLHttpRequest();
+        rr.open('GET', this.url + req, false);
+        rr.send(null);
+        if (rr.status == 200)
+            return rr.responseText;
+        else
+            return rr.status;
+    }
+};
+
+var aStr;
 var opStr;
+var sCalc = new ServerCalc('http://localhost:8370/?');
 
 function fNum(p) {
     aTxt.value += p.value;
@@ -8,7 +29,7 @@ function fOp(p) {
     if (p.value == '=') {
         var aNum = aStr;
         var bNum = aTxt.value;
-        var res = serverCalc(aNum, bNum, opStr);
+        var res = sCalc.Calculate(aNum, bNum, opStr);
         aTxt.value = res;
     }
     else {
@@ -16,16 +37,4 @@ function fOp(p) {
         aTxt.value = "";
         opStr = p.value;
     }
-}
-
-function serverCalc(a, b, op) {
-    if (op === "+") { op = "plus"; }
-    var req = "num1=" + a + "&num2=" + b + "&opr=" + op;
-    var rr = new XMLHttpRequest();
-    rr.open('GET', 'http://localhost:8370/?' + req, false);
-    rr.send(null);
-    if (rr.status == 200)
-        return rr.responseText;
-    else
-        return rr.status;
 }
