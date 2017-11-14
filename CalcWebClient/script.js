5f08bbc06ce35b6284edc5176f6e01557f58dd54
Клименко Владимир <klimbox@gmail.com>
2017-11-14 22:05:37 +0200

    #19.4: rename function + fix paths to style and script files

--- CalcWebClient/script.js
+++ CalcWebClient/script.js
@@ -8,7 +8,7 @@ function fOp(p) {
     if (p.value == '=') {
         var aNum = aStr;
         var bNum = aTxt.value;
-        var res = calcN(aNum, bNum, opStr);
+        var res = serverCalc(aNum, bNum, opStr);
         aTxt.value = res;
     }
     else {
@@ -17,7 +17,8 @@ function fOp(p) {
         opStr = p.value;
     }
 }
-function calcN(a, b, op) {
+
+function serverCalc(a, b, op) {
     if (op === "+") { op = "plus"; }
     var req = "num1=" + a + "&num2=" + b + "&opr=" + op;
     var rr = new XMLHttpRequest();
